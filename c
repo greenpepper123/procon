4c48c3c64137efc3db3960e98810688cad0f2e17
Nobuki Sugito <nobupiiman@gmail.com>
2020-07-12 19:41:15 +0900

    initial commit skeleton and Makefile

---
+++ c
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp $(dirname $0)/codes/skeleton.cpp $(dirname $0)/codes/$1.cpp
+
---
+++ codes/skeleton.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main() {
+    
+
+    return 0;
+}
